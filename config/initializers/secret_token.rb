17be3f6d31ca8c83552a405202592773e118bbe2
Raed <raed@your-majesty.com>
2011-10-29 16:23:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skype::Application.config.secret_token = '619f684c2961b56d39cd7e92f678467d0133bf72cadcc02389395aecc55ff2d49566fc5d84a90e5240b5a7fdd4f3a6b9946311b0ca5a7e298533280857039416'
